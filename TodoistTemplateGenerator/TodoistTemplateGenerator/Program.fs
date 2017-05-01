a8f09336cff908c7b6c7cf9b0d07688d00fde78b
Rahul P Nath <rahulpnath@gmail.com>
2017-05-02 06:24:43 +1000

    Calculate start in days

--- TodoistTemplateGenerator/TodoistTemplateGenerator/Program.fs
+++ TodoistTemplateGenerator/TodoistTemplateGenerator/Program.fs
@@ -1,9 +1,23 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿namespace TodoistTemplateGenerator
 
-open FSharp.Data
+module Main =
+    open FSharp.Data
+    open System
 
-[<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+    let startInDays (now:DateTime) (newDate:DateTime) = 
+        match newDate >= now with
+        | true -> Success((newDate - now).TotalDays)
+        | false -> Failure("New date must be greater than now")
+
+    [<EntryPoint>]
+    let main argv = 
+        printfn "%A" argv
+        let cla = ArgParser.parse (argv |> Seq.toList) //TODO: How to avoid this?
+        let template = Template.Load("Template.csv")
+
+        // Get start date correction days
+        // For each row
+            // Parse date in template to a type
+            // Apply the correction date and get string
+
+        0 // return an integer exit code
